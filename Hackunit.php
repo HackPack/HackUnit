c7d9865a2ef20449eeb6f4b29c69d029041978cf
Brian Scaturro <scaturrob@gmail.com>
2014-05-16 02:20:48 +0000

    remove loader config from Hackunit.php

--- Hackunit.php
+++ Hackunit.php
@@ -1,3 +1,6 @@
 <?hh //partial
+/**
+ * no longer needed dude to introduction of Tests namespace,
+ * however this file is left to demonstrate its use
+ */
 $autoloader = include(__DIR__ . '/vendor/autoload.php');
-$autoloader->add('HackUnit', __DIR__ . '/test');
