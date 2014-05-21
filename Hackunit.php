5bfb949f001fd56bc2964aa4adeef30316228d5b
Brian Scaturro <scaturrob@gmail.com>
2014-05-21 00:01:49 -0400

    Update Hackunit.php

--- Hackunit.php
+++ Hackunit.php
@@ -1,6 +1,6 @@
 <?hh //partial
 /**
- * no longer needed dude to introduction of Tests namespace,
+ * no longer needed due to introduction of Tests namespace,
  * however this file is left to demonstrate its use
  */
 $autoloader = include(__DIR__ . '/vendor/autoload.php');
