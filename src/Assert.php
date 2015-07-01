bcea9736092022ff62c0a78c1d1d040690543aed
Isaac Leinweber <brotchen@gmail.com>
2015-07-01 00:12:56 +0000

    Simple factory methods don't need to be tested.

--- src/Assert.php
+++ src/Assert.php
@@ -4,6 +4,7 @@ namespace HackPack\HackUnit;
 
 final class Assert implements Contract\Assert
 {
+    <<nocover>>
     public static function build(
         Vector<Event\FailureListener> $failureListeners,
         Vector<Event\SkipListener> $skipListeners,
@@ -12,6 +13,8 @@ final class Assert implements Contract\Assert
     {
         return new static($failureListeners, $skipListeners, $successListeners);
     }
+
+    <<nocover>>
     public function __construct(
         private Vector<Event\FailureListener> $failureListeners,
         private Vector<Event\SkipListener> $skipListeners,
@@ -20,31 +23,37 @@ final class Assert implements Contract\Assert
     {
     }
 
+    <<nocover>>
     public function bool(bool $context) : Contract\Assertion\BoolAssertion
     {
         return new Assertion\BoolAssertion($context, $this->failureListeners, $this->successListeners);
     }
 
+    <<nocover>>
     public function int(int $context) : Contract\Assertion\NumericAssertion<int>
     {
         return new Assertion\NumericAssertion($context, $this->failureListeners, $this->successListeners);
     }
 
+    <<nocover>>
     public function float(float $context) : Contract\Assertion\NumericAssertion<float>
     {
         return new Assertion\NumericAssertion($context, $this->failureListeners, $this->successListeners);
     }
 
+    <<nocover>>
     public function string(string $context) : Contract\Assertion\StringAssertion
     {
         return new Assertion\StringAssertion($context, $this->failureListeners, $this->successListeners);
     }
 
+    <<nocover>>
     public function whenCalled((function():void) $context) : Contract\Assertion\CallableAssertion
     {
         return new Assertion\CallableAssertion($context, $this->failureListeners, $this->successListeners);
     }
 
+    <<nocover>>
     public function mixed(mixed $context) : Contract\Assertion\MixedAssertion
     {
         return new Assertion\MixedAssertion($context, $this->failureListeners, $this->successListeners);
