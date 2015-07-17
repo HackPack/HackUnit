ff8ee72122745999d73231738ec50e37ba18fe9c
Isaac Leinweber <brotchen@gmail.com>
2015-07-17 14:49:08 +0000

    Clean up standard use.  Exit with 1 if any failures occurred.

--- src/HackUnit.php
+++ src/HackUnit.php
@@ -4,6 +4,8 @@ namespace HackPack\HackUnit;
 
 final class HackUnit
 {
+    private static bool $failures = false;
+
     public static function fromCli(string $projectDir) : void
     {
         /*
@@ -40,18 +42,33 @@ final class HackUnit
          */
         $testRunner = new Test\Runner(class_meth(Assert::class, 'build'));
 
+        // Allow us to set the exit code
+        $testRunner->onFailure($event ==> {self::$failures = true;});
+
+        // Allow the reporter to listen
         $testRunner->onFailure(inst_meth($testReporter, 'reportFailure'));
         $testRunner->onSkip(inst_meth($testReporter, 'reportSkip'));
         $testRunner->onSuccess(inst_meth($testReporter, 'reportSuccess'));
         $testRunner->onPass(inst_meth($testReporter, 'reportPass'));
-
-        $testRunner->onRunStart(inst_meth($testReporter, 'identifyPackage'));
-        $testRunner->onRunStart(inst_meth($testReporter, 'startTiming'));
-
-        $testRunner->onRunEnd(inst_meth($testReporter, 'displaySummary'));
-
         $testRunner->onUncaughtException(inst_meth($testReporter, 'reportUntestedException'));
 
+        // Identify the package before running tests
+        $testRunner->onRunStart(inst_meth($testReporter, 'identifyPackage'));
+
+        // Start timing after identification
+        $testRunner->onRunStart(inst_meth($testReporter, 'startTiming'));
+
+        // Stop timing after tests
+        $testRunner->onRunEnd(inst_meth($testReporter, 'displaySummary'));
+
+
+        // LET'S DO THIS!
         $testRunner->run($testLoader->testSuites());
+
+        // Exit codes FTW
+        if(self::$failures) {
+            exit(1);
+        }
+        exit(0);
     }
 }
