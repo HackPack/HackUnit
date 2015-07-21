1af1e312e5fc41f7082b12c7bbf3979d7347ba41
Isaac Leinweber <brotchen@gmail.com>
2015-07-21 00:52:37 +0000

    Complete numeric assertion tests

--- test/Assertion/NumericAssertion.php
+++ test/Assertion/NumericAssertion.php
@@ -22,27 +22,17 @@ class NumericAssertionTest
     }
 
     <<Test>>
-    public function numbersAreEqualAndExpected(Assert $assert) : void
+    public function expectEqualIsEqual(Assert $assert) : void
     {
         $a = $this->makeAssertion(1);
         $a->eq(1);
 
         $assert->int($this->successCount)->eq(1);
-        $assert->bool($this->failEvents->isEmpty())->is(true);
+        $assert->int($this->failEvents->count())->eq(0);
     }
 
     <<Test>>
-    public function numbersAreUnequalAndExpected(Assert $assert) : void
-    {
-        $a = $this->makeAssertion(1);
-        $a->not()->eq(0);
-
-        $assert->int($this->successCount)->eq(1);
-        $assert->bool($this->failEvents->isEmpty())->is(true);
-    }
-
-    <<Test>>
-    public function numbersAreEqualAndUnexpected(Assert $assert) : void
+    public function expectEqualIsGreater(Assert $assert) : void
     {
         $line = __LINE__ + 2;
         $a = $this->makeAssertion(1);
@@ -59,15 +49,14 @@ class NumericAssertionTest
             __CLASS__,
             __FILE__,
         );
-
     }
 
     <<Test>>
-    public function numbersAreUnequalAndUnexpected(Assert $assert) : void
+    public function expectEqualIsLess(Assert $assert) : void
     {
         $line = __LINE__ + 2;
         $a = $this->makeAssertion(1);
-        $a->not()->eq(1);
+        $a->eq(2);
 
         $assert->int($this->successCount)->eq(0);
         $assert->int($this->failEvents->count())->eq(1);
@@ -80,5 +69,185 @@ class NumericAssertionTest
             __CLASS__,
             __FILE__,
         );
+    }
+
+    <<Test>>
+    public function expectGreaterIsEqual(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->gt(1);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectGreaterIsGreater(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->gt(0);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
+    }
+
+    <<Test>>
+    public function expectGreaterIsLess(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->gt(2);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectGreaterOrEqualIsEqual(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->gte(1);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
+    }
+
+    <<Test>>
+    public function expectGreaterOrEqualIsGreater(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->gte(0);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
+    }
+
+    <<Test>>
+    public function expectGreaterOrEqualIsLess(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->gte(2);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectLessIsEqual(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->lt(1);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectLessIsGreater(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->lt(0);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectLessIsLess(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->lt(2);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
+    }
+
+    <<Test>>
+    public function expectLessOrEqualIsEqual(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->lte(1);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
+    }
+
+    <<Test>>
+    public function expectLessOrEqualIsGreater(Assert $assert) : void
+    {
+        $line = __LINE__ + 2;
+        $a = $this->makeAssertion(1);
+        $a->lte(0);
+
+        $assert->int($this->successCount)->eq(0);
+        $assert->int($this->failEvents->count())->eq(1);
+
+        $this->testFailureEvent(
+            $assert,
+            $this->failEvents->at(0),
+            $line,
+            __FUNCTION__,
+            __CLASS__,
+            __FILE__,
+        );
+    }
+
+    <<Test>>
+    public function expectLessOrEqualIsLess(Assert $assert) : void
+    {
+        $a = $this->makeAssertion(1);
+        $a->lte(2);
+
+        $assert->int($this->successCount)->eq(1);
+        $assert->int($this->failEvents->count())->eq(0);
     }
 }
